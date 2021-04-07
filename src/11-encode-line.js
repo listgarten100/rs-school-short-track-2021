55cb79a8dff054c46da89e20b28fa8accff79c7b
Vlad Listgarten <listgarten100@gmail.com>
2021-04-07 16:12:24 +0300

    feat: add encodeLine function

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,30 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const array = str.split('');
+  const res = [];
+  const obj = {};
+
+  array.forEach((elem, i, arr) => {
+    if (arr[i - 1] === arr[i]) {
+      obj[elem]++;
+      if (obj[elem] < 3) {
+        const ind = res.indexOf(elem);
+        delete res[ind];
+      } else {
+        const numWithLetters = `${obj[elem] - 1}${elem}`;
+        const ind = res.indexOf(numWithLetters);
+        delete res[ind];
+      }
+      res.push(obj[elem] + elem);
+    } else {
+      res.push(elem);
+      obj[elem] = 1;
+    }
+  });
+
+  return res.join('');
 }
 
 module.exports = encodeLine;
